7185d7e3845f2f90911d7ff2867e6bf0c142d5b6
MG Johnson <cry-havoc22@hushmail.com>
2018-12-14 20:14:51 +0000

    update models and add csv files to daily report

--- Today_Systems_Model.R
+++ Today_Systems_Model.R
@@ -10,6 +10,7 @@ library(randomForest)
 library(kernlab)
 
 
+
 #bfPLMod <- readRDS("Systems_MARS_BFSPPL_Model.RDS")
 
 bfNNPLMod <- readRDS("Systems_NN_BFSPPL_Model_v50.RDS")
@@ -21,7 +22,7 @@ bfXGB_PL_Mod <- readRDS("XGB_Linear_Systems_BFPL_Model_v50.RDS")
 bfXGBMod <- readRDS("XGB_Systems_Model_Prob_V50.RDS")
 
 
-todaySQ <- read_csv(file.choose(), col_names = T)
+todaySQ <- read_csv("Today_All_System_Qualifiers.csv", col_names = T)
 
 summary(todaySQ)
 
@@ -95,16 +96,49 @@ todaySQ2$Model_Preds_V50 <- V50_Mod_Preds
 todaySQ2$SVM_Model_Preds <- SVM_Mod_Preds
 todaySQ2$MARS_Model_Preds <- MARS_Mod_Preds[,1]
 
-todaySQ2ModelQuals <- todaySQ2 %>% 
+
+todaySQ2 <- todaySQ2 %>% 
   rename(Model_Win_Prob = Win_Prob) %>% 
   mutate(Model_Value_Odds = 1/Model_Win_Prob,
          Model_Val_Ratio = BetFairSPForecastWinPrice/Model_Value_Odds,
-         Models_Avg = (XGB_Pred + NN_Pred + RF_Pred)/3,
+         Base_Models_Avg = (XGB_Pred + NN_Pred + RF_Pred)/3,
          V_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50)/3,
-         Final_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50 + SVM_Model_Preds + MARS_Model_Preds)/5) %>%
-  select(Time24Hour, Meeting, Horse, System_Name, Final_Models_Avg, SVM_Model_Preds, MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, V_Models_Avg, 
-         Models_Avg, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>% 
-  filter(Model_Preds_V50 > 0) %>% 
+         Final_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50 + SVM_Model_Preds + MARS_Model_Preds)/5,
+         All_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50 + SVM_Model_Preds + MARS_Model_Preds +
+                             XGB_Pred + NN_Pred + RF_Pred)/8) %>%
+  arrange(Time24Hour, Meeting, Horse)
+
+posModels <- todaySQ2 %>% 
+  mutate(All_Model_Pos = if_else(All_Models_Avg > 0, 1, 0),
+         Final_Model_Pos = if_else(Final_Models_Avg > 0, 1, 0),
+         V_Model_Pos = if_else(V_Models_Avg > 0, 1, 0),
+         Base_Model_Pos = if_else(Base_Models_Avg > 0, 1, 0),
+         SVM_Model_Pos = if_else(SVM_Model_Preds > 0, 1, 0),
+         MARS_Model_Pos = if_else(MARS_Model_Preds > 0, 1, 0),
+         V50_Model_Pos = if_else(Model_Preds_V50 > 0, 1, 0),
+         V30_Model_Pos = if_else(Model_Preds_V30 > 0, 1, 0),
+         V20_Model_Pos = if_else(Model_Preds_V20 > 0, 1, 0),
+         XGB_Model_Pos = if_else(XGB_Pred > 0, 1, 0),
+         NN_Model_Pos = if_else(NN_Pred > 0, 1, 0),
+         RF_Model_Pos = if_else(RF_Pred > 0, 1, 0)) %>% 
+  select(All_Model_Pos, Final_Model_Pos, V_Model_Pos, Base_Model_Pos, SVM_Model_Pos, MARS_Model_Pos, V50_Model_Pos, V30_Model_Pos,
+         V20_Model_Pos, XGB_Model_Pos, NN_Model_Pos, RF_Model_Pos)
+  
+
+posModels$Model_Score = apply(posModels, MARGIN = 1, FUN = sum)
+
+todaySQ2$Model_Score = posModels$Model_Score
+
+todaySQ2 <- todaySQ2 %>% 
+  filter(Model_Score > 0)
+
+
+
+todaySQ2ModelQuals <- todaySQ2 %>% 
+  select(Time24Hour, Meeting, Horse, System_Name, Model_Score, All_Models_Avg, Final_Models_Avg, V_Models_Avg, Base_Models_Avg, SVM_Model_Preds, 
+         MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, 
+         Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
+  filter(All_Models_Avg > 0) %>% 
   arrange(Time24Hour, Meeting, Horse)
 
 todaySQ2ModelQuals
@@ -112,23 +146,18 @@ todaySQ2ModelQuals
 View(todaySQ2ModelQuals)
 
 
-write_csv(todaySQ2ModelQuals, paste0("Today_Model_Sys_Quals_", today$Date[1], ".csv"))
+write_csv(todaySQ2ModelQuals, paste0("Today_Model_Sys_Quals_", Sys.Date(), ".csv"))
 
 todaySQ2All <- todaySQ2 %>% 
-  rename(Model_Win_Prob = Win_Prob) %>% 
-  mutate(Model_Value_Odds = 1/Model_Win_Prob,
-         Model_Val_Ratio = BetFairSPForecastWinPrice/Model_Value_Odds,
-         Models_Avg = (XGB_Pred + NN_Pred + RF_Pred)/3,
-         V_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50)/3,
-         Final_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50 + SVM_Model_Preds + MARS_Model_Preds)/5) %>%
-  select(Time24Hour, Meeting, Horse, System_Name, Final_Models_Avg, SVM_Model_Preds, MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, V_Models_Avg, 
-         Models_Avg, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
+  select(Time24Hour, Meeting, Horse, System_Name, Model_Score, All_Models_Avg, Final_Models_Avg, V_Models_Avg, Base_Models_Avg, SVM_Model_Preds, 
+         MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, 
+         Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
   arrange(Time24Hour, Meeting, Horse)
   
 
 View(todaySQ2All)  
 
-write_csv(todaySQ2All, paste0("Today_Model_Sys_Ratings_", today$Date[1], ".csv"))
+write_csv(todaySQ2All, paste0("Today_Model_Sys_Ratings_", Sys.Date(), ".csv"))
 
 # 
 # todaySQ2Hcp <- todaySQ2 %>% 
@@ -146,42 +175,50 @@ write_csv(todaySQ2All, paste0("Today_Model_Sys_Ratings_", today$Date[1], ".csv")
 # write_csv(todaySQ2Hcp, paste0("Today_Model_Hcp_Ratings_", today$Date[1], ".csv"))
 
 todaySQ2Elite <- todaySQ2 %>% 
-  rename(Model_Win_Prob = Win_Prob) %>% 
-  mutate(Model_Value_Odds = 1/Model_Win_Prob,
-         Model_Val_Ratio = BetFairSPForecastWinPrice/Model_Value_Odds,
-         Models_Avg = (XGB_Pred + NN_Pred + RF_Pred)/3,
-         V_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50)/3,
-         Final_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50 + SVM_Model_Preds + MARS_Model_Preds)/5) %>%
-  select(Time24Hour, Meeting, Horse, System_Name, Final_Models_Avg, SVM_Model_Preds, MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, V_Models_Avg, 
-         Models_Avg, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
-  filter(Final_Models_Avg > 0, Models_Avg > 0) %>%
+  select(Time24Hour, Meeting, Horse, System_Name, Model_Score, All_Models_Avg, Final_Models_Avg, V_Models_Avg, Base_Models_Avg, SVM_Model_Preds, 
+         MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, 
+         Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
+  filter(Base_Models_Avg > 0, Final_Models_Avg > 0) %>%
   arrange(Time24Hour, Meeting, Horse)
 
 todaySQ2Elite
 
 View(todaySQ2Elite)
 
-write_csv(todaySQ2Elite, paste0("Today_Elite_Model_Quals_", today$Date[1], ".csv"))
+write_csv(todaySQ2Elite, paste0("Today_Elite_Model_Quals_", Sys.Date(), ".csv"))
 
 
 todaySQ2DualAvg <- todaySQ2 %>% 
-  rename(Model_Win_Prob = Win_Prob) %>% 
-  mutate(Model_Value_Odds = 1/Model_Win_Prob,
-         Model_Val_Ratio = BetFairSPForecastWinPrice/Model_Value_Odds,
-         Models_Avg = (XGB_Pred + NN_Pred + RF_Pred)/3,
-         V_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50)/3,
-         Final_Models_Avg = (Model_Preds_V20 + Model_Preds_V30 + Model_Preds_V50 + SVM_Model_Preds + MARS_Model_Preds)/5) %>%
-  select(Time24Hour, Meeting, Horse, System_Name, Final_Models_Avg, SVM_Model_Preds, MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, V_Models_Avg, 
-         Models_Avg, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
-  filter(V_Models_Avg > 0, Models_Avg > 0) %>%
+  select(Time24Hour, Meeting, Horse, System_Name, Model_Score, All_Models_Avg, Final_Models_Avg, V_Models_Avg, Base_Models_Avg, SVM_Model_Preds, 
+         MARS_Model_Preds, Model_Preds_V50, Model_Preds_V30, Model_Preds_V20, XGB_Pred, NN_Pred, RF_Pred, Model_Value_Odds, 
+         Model_Val_Ratio, Handicap, Ratings_Range, everything()) %>%
+  filter(V_Models_Avg > 0, Base_Models_Avg > 0) %>%
   arrange(Time24Hour, Meeting, Horse)
 
 todaySQ2DualAvg
 
 View(todaySQ2DualAvg)
 
-#write_csv(todaySQ2DualAvg, paste0("Today_DualAvg_Model_Quals_", today$Date[1], ".csv"))
+write_csv(todaySQ2DualAvg, paste0("Today_DualAvg_Model_Quals_", Sys.Date(), ".csv"))
+
+
+todayModVal <- todaySQ2 %>% 
+  mutate(UKVO_v_MVO = ValueOdds_BetfairFormat/Model_Value_Odds) %>% 
+  select(Time24Hour, Meeting, Horse, System_Name, UKVO_v_MVO, Model_Value_Odds, ValueOdds_BetfairFormat, BetFairSPForecastWinPrice,
+         Model_Score, All_Models_Avg, Final_Models_Avg, V_Models_Avg, Base_Models_Avg, Handicap) %>% 
+  filter(UKVO_v_MVO >= 1.25) %>% 
+  arrange(Time24Hour, Meeting, Horse)
+
+View(todayModVal)
 
 
 
+
+
+
+
+
+
+
+  
   
